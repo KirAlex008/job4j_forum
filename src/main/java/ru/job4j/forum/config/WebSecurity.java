847383b9dc9d66f4c5fd3bcf7e142783696df6ed
AlexKir008 <kirshaav@yandex.ru>
2021-10-18 22:25:43 +0300

    0. Spring test [#6881] bot errors correction 8

--- src/main/java/ru/job4j/forum/config/WebSecurity.java
+++ src/main/java/ru/job4j/forum/config/WebSecurity.java
@@ -16,8 +16,8 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-    @Autowired
-    PasswordEncoder passwordEncoder;
+/*    @Autowired
+    PasswordEncoder passwordEncoder;*/
 
     @Autowired
     DataSource ds;
