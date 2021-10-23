6f22aa440d4bf9a678a5fc8dc28079f201dd3c08
AlexKir008 <kirshaav@yandex.ru>
2021-10-23 16:14:10 +0300

    0. Spring test [#6881] bot errors correction 21

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS authorities (
-                                           id serial primary key,
-                                           authority VARCHAR(50) NOT NULL
+                             id serial primary key,
+                             authority VARCHAR(50) NOT NULL unique
 );
-
 CREATE TABLE IF NOT EXISTS users (
-                                     id serial primary key,
-                                     authority_id int not null references authorities(id),
-                                     enabled boolean default true,
-                                     password VARCHAR(100) NOT NULL,
-                                     username VARCHAR(50) NOT NULL
+                       id serial primary key,
+                       username VARCHAR(50) NOT NULL unique,
+                       password VARCHAR(100) NOT NULL,
+                       enabled boolean default true,
+                       authority_id int not null references authorities(id)
 );
 
-CREATE TABLE IF NOT EXISTS posts (
-                                     id serial primary key,
-                                     name VARCHAR(2000) NOT NULL,
-                                     description text,
-                                     created timestamp without time zone default now()
+create table IF NOT EXISTS posts (
+                       id serial primary key,
+                       name varchar(2000),
+                       description text,
+                       created timestamp without time zone not null default now(),
+                       user_id int references users(id)
 );
