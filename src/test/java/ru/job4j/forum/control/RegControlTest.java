5b14224144e747e59d572f24ee8c8e0eb790880d
AlexKir008 <kirshaav@yandex.ru>
2021-10-23 17:14:14 +0300

    0. Spring test [#6881] bot errors correction 31

--- src/test/java/ru/job4j/forum/control/RegControlTest.java
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
 import ru.job4j.forum.service.PostService;
@@ -19,6 +20,7 @@ import ru.job4j.forum.service.UserService;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 
 class RegControlTest {
     @Autowired
