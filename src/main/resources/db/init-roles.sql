9d718d76336108ccf696e5ee655e1e9c372d12c8
AlexKir008 <kirshaav@yandex.ru>
2021-10-25 19:25:05 +0300

    0. Spring Liquibase [#302330] bot errors correction 10

--- src/main/resources/db/init-roles.sql
+++ src/main/resources/db/init-roles.sql
@@ -1,4 +1,3 @@
 insert into authorities(authority) values ('ROLE_USER');
 insert into authorities(authority) values ('ROLE_ADMIN');
-insert into "users" (username, password, enabled, authority_id) values ('user', '$2a$10$YU0VQz3.y3FLVPPGjoCdhufzLsiYmb1VTty8ARIlVHq7NYiUFYBQi', true,
-        (select id from "authorities" where authority = 'ROLE_USER'));
+insert into "users" (username, password, enabled, authority_id) values ('user', '$2a$10$YU0VQz3.y3FLVPPGjoCdhufzLsiYmb1VTty8ARIlVHq7NYiUFYBQi', true, 1);
